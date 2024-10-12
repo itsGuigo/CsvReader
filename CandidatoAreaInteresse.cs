89c678692c2c41ce80be2378c8a76fe5c9dc3b87
Guilherme Gomes do Nascimento <98121832+itsGuigo@users.noreply.github.com>
2024-10-12 16:09:09 -0300

    Add files via upload

---
+++ CandidatoAreaInteresse.cs
@@ -0,0 +1,15 @@
+﻿namespace CsvReader.Classes
+{
+    public class CandidatoAreaInteresse
+    {
+        public int IdProjetoCandidato { get; set; }
+        public int IdAreaInteresse1 { get; set; }
+        public string? IdAreaInteresse2 { get; set; }
+        public string? IdAreaInteresse3 { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public int IdUsuarioAlteracao { get; set; }
+        public int IdUsuarioCriacao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
